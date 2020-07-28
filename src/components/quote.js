badbc971b42a5c93186bd1a76e861f18a1e19064
Khris Punzalan <khrispunzalan@gmail.com>
2020-07-28 13:11:13 -0500

    Quote displayed correctly

--- src/components/quote.js
+++ src/components/quote.js
@@ -2,9 +2,22 @@ class Quote {
   constructor() {
     this.adapter = new QuoteAdapter();
     this.getQuote();
+    this.initBindingAndEventListeners();
+  }
+
+  initBindingAndEventListeners() {
+    // this.quoteContent = document.querySelector('#quote-content');
+    this.quoteParagraph = document.querySelector('#quote-paragraph');
+    this.quoteAuthor = document.querySelector('#quote-author');
+    this.getButton = document
+      .querySelector('#new-quote-button')
+      .addEventListener('click', this.getQuote.bind(this));
   }
 
   getQuote() {
-    this.adapter.getRandomQuote();
+    this.adapter.getRandomQuote().then((quote) => {
+      this.quoteParagraph.innerText = quote.content;
+      this.quoteAuthor.innerText = quote.author;
+    });
   }
 }
